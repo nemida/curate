b5c0479c0bdacb29455dfb85c9089a16f75bd338
Nemida <144930141+Nemida@users.noreply.github.com>
2026-08-31 05:55:30 +0530

    Start dev server in Playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,10 @@
 import { defineConfig } from '@playwright/test';
 export default defineConfig({
   use: { baseURL: 'http://localhost:3000' },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  }
 });
