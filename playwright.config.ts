baa93ce8ef78ecbe7641f7843d73971d4745ca6b
Nemida <144930141+Nemida@users.noreply.github.com>
2026-08-31 05:52:23 +0530

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-﻿import { defineConfig } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 export default defineConfig({
-  use: { baseURL: 'http://localhost:3000' }
+  use: { baseURL: 'http://localhost:3000' },
 });
